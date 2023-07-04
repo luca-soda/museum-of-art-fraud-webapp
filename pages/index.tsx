2cd9d613f4bdd1ec42954938e57ccb51a88c3e55
Luca Soda <luca.soda@gmail.com>
2023-07-04 21:36:10 +0200

    deploy

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <QrScanner onDecode={(result) => console.log(result)} onError={function (error: Error): void {}} />
+      <QrScanner onDecode={(result) => alert(result)} onError={function (error: Error): void {}} />
     </>
   )
 }
